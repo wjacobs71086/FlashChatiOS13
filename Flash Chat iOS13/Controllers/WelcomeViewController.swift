4a83f2afe6ca07588d264da8453876e6a7f5d1c5
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-10 11:31:06 -0700

    added Firebase Auth and FireStore to project dependencies

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,26 +7,15 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-
-        
-        for letter in titleText {
-            // If you increment the interval it will, with each loop, make the delay longer. This gives the appearence of a typewriter.
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
     }
     
 
--- Podfile
+++ Podfile
@@ -1,7 +1,10 @@
 platform :ios, '13.0'
 
 target 'Flash Chat iOS13' do
-
+  use_frameworks!
   # Pods for Flash Chat iOS13
+  # To tell a SPECIFIC version use '~> 0.4.0' at the end in the single quotes
 pod 'CLTypingLabel'
+pod 'Firebase/Auth'
+pod 'Firebase/Firestore'
 end
