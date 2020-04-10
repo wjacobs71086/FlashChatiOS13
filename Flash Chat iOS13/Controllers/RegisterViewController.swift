205401f7322aca430d1784e0c310fa9390bdf140
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-10 11:53:57 -0700

    registration is working and linked with firebase

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        errorLabel.text = ""
+    }
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text , let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let err = error {
+                    self.errorLabel.text = err.localizedDescription
+                    print("This is an error.",err.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
+        
+        
     }
     
 }
